d885a882c2352793ceb6baaeef3c0d188200fbfa
christopherwilson <christopher.gestwicki@gmail.com>
2023-11-26 17:52:52 +0000

    Added documentation comments

--- code/src/parser.py
+++ code/src/parser.py
@@ -7,8 +7,22 @@ from math import ceil, sqrt
 
 class Parser:
 
-
     def __init__(self, file_path: str):
+        """
+        Parses a representation of a commutative diagram.
+
+        Each line of the commutative diagram representation should represent a function in the diagram, and be of
+        the form::
+
+            {Domain}{Codomian}{Function}
+
+        For example; the function ``f: A -> B`` should be represented by::
+
+            {A}{B}{f}
+
+        ``Domain``, ``Codomain`` and ``Function`` cannot be the empty string, but they may contain \"{\" and \"}\".
+        :param file_path: Location of the text representation of the commutative diagram.
+        """
         self.graph: nx.DiGraph = nx.DiGraph()
         with (open(file_path, 'r') as f):
             labels = [""] * 3
@@ -17,7 +31,7 @@ class Parser:
                 if line[0] == "%":  # lets us comment
                     continue
                 if num_labels < 3:
-                    raise Exception("Invalid")
+                    raise Exception("Invalid number of labels")
                 num_labels = 0
                 labels[2] = ""  # clearing the maps name
                 j = 0
@@ -40,7 +54,13 @@ class Parser:
 
     @staticmethod
     def extract_label(line: str, start_pos: int) -> tuple[str, int]:
-        unmatched_brackets: int = 1
+        """
+        Extracts the label attached to a domain, codomain, or function in the commutative diagram text representation.
+        :param line: line of text containing the label, should be of the form ``"some text{Label}some more text"``.
+        :param start_pos: the position of the first character of the Label.
+        :return: ``({Label},`` the position of the closing "``}``"``)``
+        """
+        unmatched_brackets: int = 1  # we should always start with an unmatched bracket
         i: int = start_pos
         while unmatched_brackets != 0:
             if line[i] == "{":
@@ -48,7 +68,7 @@ class Parser:
             elif line[i] == "}":
                 unmatched_brackets -= 1
             i += 1
-        return line[start_pos-1:i], i
+        return line[start_pos - 1:i], i  # the start position is just after the opening {, so we need to decrement
 
     def to_codi(self) -> str:
         node_matrix = self.place_nodes()
@@ -64,6 +84,12 @@ class Parser:
 
     @staticmethod
     def lists_to_latex_matrix(lst: list[list[Any]]) -> str:
+        """
+        Converts a matrix represented by a list of lists to the LaTeX representation of a matrix.
+        :param lst: a list of lists representing a matrix. Each sub-list should be of equal length.
+        :return: A string representing ``lst`` as a latex matrix
+        """
+        # TODO replace this with code that uses .join
         matrix = [""] * (len(lst) * len(lst[0]))
         i = 0
         for line in lst:
@@ -78,6 +104,10 @@ class Parser:
         return " ".join(matrix)
 
     def place_nodes(self) -> list[list[str]]:
+        """
+        Positions all the nodes of the graph stored in ``self.graph`` in a matrix.
+        :return: a list of lists containing all the labels of the nodes, and potentially some empty strings
+        """
         # TODO: better algo
         nodes = self.graph.nodes
         matrix_size: int = ceil(sqrt(len(nodes)))
@@ -91,17 +121,21 @@ class Parser:
                 line += 1
         return ob_matrix
 
-    def to_func_comps(self):
+    def to_func_comps(self) -> str:
+        """
+        :return: a representation of the composed functions that are equal to each other
+        """
+        # The do
         # noinspection PyCallingNonCallable
-        doms = [node for node in self.graph.nodes if self.graph.out_degree(node) > 1]
+        domains = [node for node in self.graph.nodes if self.graph.out_degree(node) > 1]
         # noinspection PyCallingNonCallable
-        codoms = [node for node in self.graph.nodes if self.graph.in_degree(node) > 1]
+        codomains = [node for node in self.graph.nodes if self.graph.in_degree(node) > 1]
 
         eqs = []
-        for dom in doms:
-            for codom in codoms:
+        for domain in domains:
+            for codomain in codomains:
                 funcs = []
-                paths = list(nx.algorithms.all_simple_edge_paths(self.graph, dom, codom))
+                paths: list[list[tuple]] = list(nx.algorithms.all_simple_edge_paths(self.graph, domain, codomain))
                 if len(paths) <= 1:
                     continue
                 for path in paths:
@@ -112,7 +146,12 @@ class Parser:
         return "\n".join(eqs)
 
     def path_to_func_comp(self, path: list[tuple]):
-        funcs = [""]*len(path)
+        """
+        Converts a list of function domain and codomains to the equivalent composition of functions.
+        :param path: a list of 2-tuples, containing the domain then codomain of a function in the diagram.
+        :return: the function composition equivalent to the path.
+        """
+        funcs = [""] * len(path)
         i = 0
         for edge in reversed(path):
             funcs[i] = self.graph.get_edge_data(edge[0], edge[1]).get("name")
